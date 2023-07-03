4801c2991872d15832b923c96923cb05636db032
Muhammad Naeem Khan <inaeem@protonmail.com>
2023-07-03 13:51:11 +0500

    post `index.jinja:form` to `/generator`"

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask
+from services.PasswordGenerator import PasswordGenerator
 from flask import render_template
+from flask import request
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -8,6 +10,20 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.jinja')
+
+
+@app.route('/generator', methods=["POST"])
+def generator():
+    length = 6
+    try:
+        length = int(request.form.get('password-length'))
+    except Exception as e:
+        print(e)
+
+    generator = PasswordGenerator(password_length=length)
+    password = generator.generate()
+
+    return dict(password=password)
 
 
 if __name__ == '__main__':
