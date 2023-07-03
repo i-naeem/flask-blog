b7969e2617f484ac12461ff203ffdcfe50aa4f06
Muhammad Naeem Khan <inaeem@protonmail.com>
2023-07-03 16:21:55 +0500

    Update `nav` show active page

--- app.py
+++ app.py
@@ -9,19 +9,19 @@ app = Flask(__name__)
 # Home Route
 @app.route('/')
 def home():
-    return render_template('index.jinja', current_route="/about")
+    return render_template('index.jinja', title="")
 
 
 # About
 @app.route('/about')
 def about():
-    return render_template('about.jinja')
+    return render_template('about.jinja', title="About")
 
 
 # Contact
 @app.route('/contact')
 def contact():
-    return render_template('contact.jinja')
+    return render_template('contact.jinja', title="Contact")
 
 
 @app.route('/generator', methods=["POST"])
